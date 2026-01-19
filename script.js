894324bafeafb8179a9249386e648349c75f7c36
ManWithDreams <103361329+WalterWhite123@users.noreply.github.com>
2026-01-19 13:28:41 +0100

    Add timer function
    I used setInterval() and clearInterval() instead
     of using time methods

--- script.js
+++ script.js
@@ -7,14 +7,6 @@
 // Mettre un timer 
 
 
-// Gives a random interger between 0 and 1 000 000
-function random_number(){
-    return Math.floor(Math.random()*1000001);
-}
-
-// console.log(random_number());
-
-
 
 function game(random_number){
     let guess;
@@ -27,6 +19,37 @@ function game(random_number){
             alert("more");
         }
     }
+
+    
 }
 
-game(random_number());
+// Gives a random interger between 0 and 1 000 000
+function random_number(){
+    return Math.floor(Math.random()*1000001);
+}
+
+
+
+
+
+
+//Input : seconds of the timer 
+//Output : 1 when it is finished
+function timer(seconds){
+    let sec = seconds;
+    console.log(sec);
+    let timer = setInterval(function(){ 
+    sec--;
+    console.log(sec);
+    if (sec <= 0){
+        clearInterval(timer);
+    }
+},1000)
+    if (sec < 0){
+        return 1;
+    }
+}
+
+
+
+
