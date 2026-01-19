513d613f873c7ee770b36743147727a88e9ebae8
ManWithDreams <103361329+WalterWhite123@users.noreply.github.com>
2026-01-19 16:37:36 +0100

    working game finally

--- script.js
+++ script.js
@@ -2,33 +2,64 @@
 // Mettre une interface UI
 
 
-// Générer un nombre aléatoire 
-// Prendre les tentatives du joueurs 
-// Mettre un timer 
-
-
+let guess = 10;
 
 function game(random_number){
-    let guess;
-    let time = 5;
-   let game = setInterval(function(){
-        console.log(time);
-        --time;
-        if (guess == random_number || time < 0){
-            game = clearInterval(game);
-        }
-        guess = prompt("Take a guess: ");
-        if (guess > random_number){
-            console.log("less")
-        }
-        else if (guess < random_number){
-            console.log("more");
-        }
+    let sec = 10;
+    chrono = document.querySelector("#chrono");
+    endPanel = document.querySelector("#endPanel");
+    hint = document.querySelector("#hints");
+    console.log(sec);
+    let timer = setInterval(function(){ 
+    sec--;
     
-    },1000);
+    chrono.textContent = sec;
+    if (sec <= 0){
+        console.log("you lost, no more time ! :)")
+        endPanel.textContent = "You lost, no more time !";
+        clearInterval(timer);
+    }
+},1000)
+// input
+input = document.querySelector("#input");
+input.addEventListener("keyup",function(e){
+    if (e.keyCode === 13){
+        guess = input.value;
+        input.value = "";
+        if (guess > random_number ){
+            hint.textContent = "less";
+        console.log("less");
+    }
+    else if (guess < random_number){
+        console.log("more");
+        hint.textContent = "more";
+    }
+    else{
+        clearInterval(timer);
+        console.log("you won");
+        endPanel.textContent = "You won";
+        chrono.textContent = "0";
+    }
+    }
+})
+//timer 
 
+//règle
     
 }
+
+playBtn = document.querySelector("#play");
+ playBtn.addEventListener("click",()=> {
+    endPanel = document.querySelector("#endPanel");
+    hint = document.querySelector("#hints");
+
+    hint.textContent = "Take a guess";
+    endPanel.textContent = "Enjoy";
+    game(30)});
+
+
+
+
 
 // Gives a random interger between 0 and 1 000 000
 function random_number(){
@@ -42,22 +73,25 @@ function random_number(){
 
 //Input : seconds of the timer 
 //Output : 1 when it is finished
-function timer(seconds){
-    let sec = seconds;
-    console.log(sec);
-    let timer = setInterval(function(){ 
-    sec--;
-    console.log(sec);
-    if (sec <= 0){
-        clearInterval(timer);
-    }
-},1000)
-    if (sec < 0){
-        return -1;
-    }
-}
+// function timer(seconds){
+//     let sec = seconds;
+//     console.log(sec);
+//     let timer = setInterval(function(){ 
+//     sec--;
+//     console.log(sec);
+//     if (sec <= 0){
+//         clearInterval(timer);
+//     }
+// },1000)
+//     if (sec < 0){
+//         return -1;
+//     }
+// }
 
 
 
 
-game(30);
+
+
+
+
